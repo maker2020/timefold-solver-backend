7027420693cf5ce18209a145ac8b77f81131a32d
胡方帅 <1838813391@qq.com>
2023-09-15 14:18:25 +0800

    添加跨域配置

--- src/main/java/com/keyvalues/optaplanner/maprouting/service/impl/SolverServiceImpl.java
+++ src/main/java/com/keyvalues/optaplanner/maprouting/service/impl/SolverServiceImpl.java
@@ -88,7 +88,8 @@ public class SolverServiceImpl implements SolverService{
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    log.error(e.getMessage());
+                    break;
                 }
             }
         }).start();
